624ce4344d389a50164b2444d76c6dd9b01b22fd
Dhayana <contatodhayana@gmail.com>
2020-05-22 23:53:34 -0300

    alterações no layout

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const inputElement = document.querySelector("#listName");
-const listElement = document.querySelector(".container-lists");
+const listContainer = document.querySelector(".container-two");
 const buttonsElement = document.querySelectorAll(".colors");
 
 let lists = [];
@@ -10,7 +10,7 @@ const addList = () => {
     let color = document.querySelector(".selected").style.backgroundColor;
 
     if(name != "") 
-        lists.push({ id, name, color, itens: [] })
+        lists.unshift({ id, name, color, items: [] })
     else 
         alert("Campo vazio!");  
 
@@ -18,36 +18,38 @@ const addList = () => {
     renderLists();
 }
 
-const addItem = (index, id, value) => {
-    if(value.trim() != "")  {
-        lists[index].itens.push({
+const addTask = (index, id, value) => {
+    let name  = value.trim();
+
+    if(name!= "")  {
+        lists[index].items.unshift({
             id: Math.random().toString(36).substring(2),
-            name: value.trim(),
+            name,
             completed: false
         });
-        renderItens(index,id);
-    }
+        renderTasks(index,id);
+    } 
     else 
         alert("Campo vazio!"); 
 }
 
-const alertConfirmation = (index) => {
-    let response = confirm("Deseja realmente deletar esta lista?");
+const alertConfirmation = (index, name) => {
+    let response = confirm(`Deseja deletar "${name}"?`);
 
-    if (response){
+    if (response) {
         lists.splice(index,1);
         renderLists();
     }
-
 }
 
-const renderItens = (index, id) => {
+const renderTasks = (index, id) => {
     let containerElement = document.getElementById(`${id}`);
     containerElement.innerHTML = "";
     
-    lists[index].itens.forEach((element, indexItem) => {
-        let containerItem = document.createElement("div");
-        containerItem.classList.add("item");     
+    lists[index].items.forEach((element, indexTask) => {
+        let containerTask = document.createElement("div");
+        containerTask.classList.add("task");  
+
         let task = document.createElement("p");
         task.style.overflowX = "auto";
         task.innerText = element.name;
@@ -56,88 +58,100 @@ const renderItens = (index, id) => {
 
         let completed = document.createElement("div");
         completed.classList.add("icons","green");
+        completed.innerHTML = "&#10003;";
         completed.addEventListener("click", () => {
-            lists[index].itens.completed = !lists[index].itens.completed;
+            lists[index].items.completed = !lists[index].items.completed;
             task.classList.toggle("completed");
         })
 
         let remove = document.createElement("div");
         remove.classList.add("icons","red");
+        remove.innerHTML = "&#10005;";
         remove.addEventListener("click", () => {
-            lists[index].itens.splice(indexItem,1);
-            renderItens(index, id);
+            lists[index].items.splice(indexTask,1);
+            renderTasks(index, id);
         })
   
         containerIcons.appendChild(completed);
         containerIcons.appendChild(remove);
-        containerItem.appendChild(task);
-        containerItem.appendChild(containerIcons);
+        
+        containerTask.appendChild(task);
+        containerTask.appendChild(containerIcons);
 
-        containerElement.appendChild(containerItem);
+        containerElement.appendChild(containerTask);
     });
 }
 
 const renderLists = () => {
-    listElement.innerHTML = "";
 
-    lists.forEach((element,index) => {   
-        let container = document.createElement("div");
-        container.classList.add("container-list");
+    if(lists.length) {
+        listContainer.style.display = "block";
+        listContainer.innerHTML = "";
 
-        let containerTitle = document.createElement("div");
-        containerTitle.classList.add("list-title");
-        containerTitle.style.backgroundColor = element.color;
-        containerTitle.addEventListener("click", () => containerListItens.classList.toggle("toggle"));
-
-        let title = document.createElement("h3");
-        title.innerText = element.name;
-
-        let iconTitle = document.createElement("div");
-        iconTitle.classList.add("icon-title");
-        iconTitle.innerText = ":";
-        iconTitle.addEventListener("click", () => {
-            alertConfirmation(index);
-        })
-
-        let containerListItens = document.createElement("div");
-        containerListItens.classList.add("container-list-itens");
-
-        let containerItens = document.createElement("div");
-        containerItens.setAttribute("id", element.id);
-        
-        let containerAddItens = document.createElement("div");
-        containerAddItens.classList.add("container-addItem");
-
-        let input = document.createElement("input");
-        input.placeholder = "Nome do Item";
-        input.addEventListener('keyup', (e) => {
-            if (e.keyCode === 13) {
-                addItem(index, containerItens.id,input.value);
+        lists.forEach((element,index) => {   
+            let container = document.createElement("div");
+            container.classList.add("list-container");
+    
+            let containerTitle = document.createElement("div");
+            containerTitle.classList.add("title-container");
+            containerTitle.style.backgroundColor = element.color;
+            
+            let title = document.createElement("h3");
+            title.innerText = element.name;
+            title.style.cursor = "pointer";
+            title.addEventListener("click", () => containerListItems.classList.toggle("toggle-hide-list"));
+    
+            let iconTitle = document.createElement("div");
+            iconTitle.classList.add("title-icon");
+            iconTitle.innerHTML = "&vellip;"
+            iconTitle.addEventListener("click", () => alertConfirmation(index, element.name))
+    
+            let containerListItems = document.createElement("div");
+            containerListItems.classList.add("list-items-container");
+    
+            let containerTasks = document.createElement("div");
+            containerTasks.setAttribute("id", element.id);
+            
+            let containerItems = document.createElement("div");
+            containerItems.classList.add("item-container");
+    
+            let input = document.createElement("input");
+            input.placeholder = "Nome do Item";
+            input.addEventListener('keyup', (e) => {
+                if (e.keyCode === 13) {
+                    addTask(index, containerTasks.id, input.value);
+                    input.value = "";
+                }
+            });
+    
+            let button = document.createElement("button");
+            button.style.backgroundColor = element.color;
+            button.textContent = "Adicionar";
+            button.addEventListener("click", () => {
+                addTask(index, containerTasks.id, input.value);
                 input.value = "";
-            }
+            });
+    
+            containerItems.appendChild(input);
+            containerItems.appendChild(button);
 
+            containerListItems.appendChild(containerItems);
+            containerListItems.appendChild(containerTasks);
+
+            containerTitle.appendChild(title);
+            containerTitle.appendChild(iconTitle);
+
+            container.appendChild(containerTitle);
+            container.appendChild(containerListItems);
+
+            listContainer.appendChild(container);
+    
+            renderTasks(index, containerTasks.id);
         });
+    }
+    else
+        listContainer.style.display = "none";
 
-        let button = document.createElement("button");
-        button.textContent = "Adicionar";
-        button.addEventListener("click", () => {
-            addItem(index, containerItens.id,input.value);
-            input.value = "";
-        });
-
-        
-        containerAddItens.appendChild(input);
-        containerAddItens.appendChild(button);
-        containerListItens.appendChild(containerAddItens);
-        containerListItens.appendChild(containerItens);
-        containerTitle.appendChild(title);
-        containerTitle.appendChild(iconTitle);
-        container.appendChild(containerTitle);
-        container.appendChild(containerListItens);
-        listElement.appendChild(container);
-
-        renderItens(index, containerItens.id);
-    });
 }
 
 const changeColor = value => {
