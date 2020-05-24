e96e9feba2ceee2297d6cef2f0da8cc244428d89
Dhayana <contatodhayana@gmail.com>
2020-05-23 22:49:52 -0300

    armazenamento e alterações no readme e layout

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# todoList
+# Lista de tarefas
+Este é um projeto com a finalidade de treinar habilidades relativas a linguagem **JavaScript**. O projeto consiste em uma lista de tarefas onde é possível inserir mais de uma lista e suas respectivas tarefas, que serão armazenadas localmente. Além disso, é possivel adicionar uma cor de fundo ao título da lista, marcar o item como concluído e removê-lo. Qualquer sugestão de melhoria será muito bem-vinda :)
+
+## Layout
+<img src="img.png" alt="imagem">
+
+## Linguagens
+* JavaScript
+* HTML
+* CSS
+
+## Executar
+**Para executar este projeto na sua máquina, siga as instruções:**
+* execute o seguinte comando no seu terminal: 
+> $ git clone https://github.com/dhayananascimento/todoList.git
+* abra a pasta que será  criada
+* abra o arquivo "index.html" com o navegador de sua preferência
--- script.js
+++ script.js
@@ -1,164 +1,184 @@
-const inputElement = document.querySelector("#listName");
-const listContainer = document.querySelector(".container-two");
-const buttonsElement = document.querySelectorAll(".colors");
+const inputElement = document.querySelector("#listName")
+const listContainer = document.querySelector(".container-two")
+const buttonElement = document.querySelectorAll(".colors")
 
-let lists = [];
+let lists = JSON.parse(localStorage.getItem('lists')) || []
 
 const addList = () => {
-    let id = Math.random().toString(36).substring(2);
-    let name = (inputElement.value).trim();
-    let color = document.querySelector(".selected").style.backgroundColor;
+    
+    let id = Math.random().toString(36).substring(2)
+    let name = (inputElement.value).trim()
+    let color = document.querySelector(".selected").style.backgroundColor
 
-    if(name != "") 
+    if(name != "") {
         lists.unshift({ id, name, color, items: [] })
+        inputElement.value = ""
+        saveToStorage()
+        renderLists() 
+        document.querySelector(".list-items-container").classList.toggle("toggle-hide-list")
+    }
     else 
-        alert("Campo vazio!");  
+        alert("Campo vazio!")  
 
-    inputElement.value = "";
-    renderLists();
+    
 }
 
 const addTask = (index, id, value) => {
-    let name  = value.trim();
+    
+    let name  = value.trim()
 
     if(name!= "")  {
         lists[index].items.unshift({
             id: Math.random().toString(36).substring(2),
             name,
             completed: false
-        });
-        renderTasks(index,id);
+        })
+        saveToStorage()
+        renderTasks(index,id)
     } 
     else 
-        alert("Campo vazio!"); 
+        alert("Campo vazio!") 
+
 }
 
 const alertConfirmation = (index, name) => {
-    let response = confirm(`Deseja deletar "${name}"?`);
+    
+    let response = confirm(`Deseja deletar "${name}"?`)
 
     if (response) {
-        lists.splice(index,1);
-        renderLists();
+        lists.splice(index,1)
+        saveToStorage()
+        renderLists()
     }
 }
 
 const renderTasks = (index, id) => {
-    let containerElement = document.getElementById(`${id}`);
-    containerElement.innerHTML = "";
+    
+    let containerElement = document.getElementById(`${id}`)
+    containerElement.innerHTML = ""
     
     lists[index].items.forEach((element, indexTask) => {
-        let containerTask = document.createElement("div");
-        containerTask.classList.add("task");  
+        let taskContainer = document.createElement("div")
+        taskContainer.classList.add("task")  
 
-        let task = document.createElement("p");
-        task.style.overflowX = "auto";
-        task.innerText = element.name;
+        let task = document.createElement("p")
+        task.style.overflowX = "auto"
+        task.innerText = element.name
+        lists[index].items[indexTask].completed ? task.classList.toggle("completed") : null
 
-        let containerIcons = document.createElement("div");
+        let iconsContainer = document.createElement("div")
 
-        let completed = document.createElement("div");
-        completed.classList.add("icons","green");
-        completed.innerHTML = "&#10003;";
+        let completed = document.createElement("div")
+        completed.classList.add("icons","green")
+        completed.innerHTML = "&#10003;"
         completed.addEventListener("click", () => {
-            lists[index].items.completed = !lists[index].items.completed;
-            task.classList.toggle("completed");
+            lists[index].items[indexTask].completed = !lists[index].items[indexTask].completed
+            task.classList.toggle("completed")
+            saveToStorage()
         })
 
-        let remove = document.createElement("div");
-        remove.classList.add("icons","red");
-        remove.innerHTML = "&#10005;";
+        let remove = document.createElement("div")
+        remove.classList.add("icons","red")
+        remove.innerHTML = "&#10005;"
         remove.addEventListener("click", () => {
-            lists[index].items.splice(indexTask,1);
-            renderTasks(index, id);
+            lists[index].items.splice(indexTask,1)
+            saveToStorage()
+            renderTasks(index, id)
         })
   
-        containerIcons.appendChild(completed);
-        containerIcons.appendChild(remove);
+        iconsContainer.appendChild(completed)
+        iconsContainer.appendChild(remove)
         
-        containerTask.appendChild(task);
-        containerTask.appendChild(containerIcons);
+        taskContainer.appendChild(task)
+        taskContainer.appendChild(iconsContainer)
 
-        containerElement.appendChild(containerTask);
-    });
+        containerElement.appendChild(taskContainer)
+    })
 }
 
 const renderLists = () => {
 
     if(lists.length) {
-        listContainer.style.display = "block";
-        listContainer.innerHTML = "";
+        listContainer.style.display = "block"
+        listContainer.innerHTML = ""
 
         lists.forEach((element,index) => {   
-            let container = document.createElement("div");
-            container.classList.add("list-container");
+            let container = document.createElement("div")
+            container.classList.add("list-container")
     
-            let containerTitle = document.createElement("div");
-            containerTitle.classList.add("title-container");
-            containerTitle.style.backgroundColor = element.color;
+            let titleContainer = document.createElement("div")
+            titleContainer.classList.add("title-container")
+            titleContainer.style.backgroundColor = element.color
             
-            let title = document.createElement("h3");
-            title.innerText = element.name;
-            title.style.cursor = "pointer";
-            title.addEventListener("click", () => containerListItems.classList.toggle("toggle-hide-list"));
+            let title = document.createElement("h3")
+            title.innerText = element.name
+            title.classList.add("list-title")
+            title.addEventListener("click", () => listItemsContainer.classList.toggle("toggle-hide-list"))
     
-            let iconTitle = document.createElement("div");
-            iconTitle.classList.add("title-icon");
-            iconTitle.innerHTML = "&vellip;"
-            iconTitle.addEventListener("click", () => alertConfirmation(index, element.name))
+            let titleIcon = document.createElement("div")
+            titleIcon.classList.add("title-icon")
+            titleIcon.innerHTML = "&vellip;"
+            titleIcon.addEventListener("click", () => alertConfirmation(index, element.name))
     
-            let containerListItems = document.createElement("div");
-            containerListItems.classList.add("list-items-container");
+            let listItemsContainer = document.createElement("div")
+            listItemsContainer.classList.add("list-items-container")
     
-            let containerTasks = document.createElement("div");
-            containerTasks.setAttribute("id", element.id);
+            let taskContainer = document.createElement("div")
+            taskContainer.setAttribute("id", element.id)
             
-            let containerItems = document.createElement("div");
-            containerItems.classList.add("item-container");
+            let itemContainer = document.createElement("div")
+            itemContainer.classList.add("item-container")
     
-            let input = document.createElement("input");
-            input.placeholder = "Nome do Item";
+            let input = document.createElement("input")
+            input.placeholder = "Nome do Item"
             input.addEventListener('keyup', (e) => {
                 if (e.keyCode === 13) {
-                    addTask(index, containerTasks.id, input.value);
-                    input.value = "";
+                    addTask(index, taskContainer.id, input.value)
+                    input.value = ""
                 }
-            });
+            })
     
-            let button = document.createElement("button");
-            button.style.backgroundColor = element.color;
-            button.textContent = "Adicionar";
+            let button = document.createElement("button")
+            button.style.backgroundColor = element.color
+            button.textContent = "Adicionar"
             button.addEventListener("click", () => {
-                addTask(index, containerTasks.id, input.value);
-                input.value = "";
-            });
+                addTask(index, taskContainer.id, input.value)
+                input.value = ""
+            })
     
-            containerItems.appendChild(input);
-            containerItems.appendChild(button);
+            itemContainer.appendChild(input)
+            itemContainer.appendChild(button)
 
-            containerListItems.appendChild(containerItems);
-            containerListItems.appendChild(containerTasks);
+            listItemsContainer.appendChild(itemContainer)
+            listItemsContainer.appendChild(taskContainer)
 
-            containerTitle.appendChild(title);
-            containerTitle.appendChild(iconTitle);
+            titleContainer.appendChild(title)
+            titleContainer.appendChild(titleIcon)
 
-            container.appendChild(containerTitle);
-            container.appendChild(containerListItems);
+            container.appendChild(titleContainer)
+            container.appendChild(listItemsContainer)
 
-            listContainer.appendChild(container);
+            listContainer.appendChild(container)
     
-            renderTasks(index, containerTasks.id);
-        });
+            renderTasks(index, taskContainer.id)
+        })
     }
     else
-        listContainer.style.display = "none";
+        listContainer.style.display = "none"
 
 }
 
 const changeColor = value => {
-    buttonsElement.forEach(element => {
+    buttonElement.forEach(element => {
         if(element.id == value.id)
-            element.classList.contains("selected")? null : element.classList.add("selected"); 
+            element.classList.contains("selected")? null : element.classList.add("selected") 
         else
-            element.classList.contains("selected")? element.classList.remove("selected"): null;
-    });
+            element.classList.contains("selected")? element.classList.remove("selected"): null
+    })
 }
+
+const saveToStorage = () => {
+    localStorage.setItem('lists', JSON.stringify(lists))
+}
+
+renderLists()
